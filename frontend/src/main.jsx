8c25447fc0045eeff27aa5861ba0114cbb9f4d60
Kingsley <ekehkingsley@yahoo.com>
2024-01-21 17:08:01 +0100

    new

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,9 @@ import App from "./App.jsx";
 import "./index.css";
 import { store } from "./app/store.js";
 import { Provider } from "react-redux";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
+
+if (process.env.NODE_ENV === "production") disableReactDevTools();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
